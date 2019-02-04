8782298989c6c11babff62971245da62720ad774
Ryan McDougall <rp.mcdougall@gmail.com>
2019-02-03 19:58:11 -0600

    implemented functors for Tree type

---
+++ src/main/scala/rm/cats/Functor.scala
@@ -0,0 +1,39 @@
+package rm.cats
+import cats._
+import cats.implicits._
+
+// TODO: Reread Functor Section
+object RunFunction extends App{
+
+  sealed trait Tree[+A]
+
+  final case class Branch[A](left: Tree[A], right: Tree[A])
+    extends Tree[A]
+
+  final case class Leaf[A](value: A) extends Tree[A]
+
+  object Tree {
+    def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
+      Branch(left, right)
+
+    def leaf[A](value: A): Tree[A] =
+      Leaf(value)
+  }
+
+  implicit val treeFunctor: Functor[Tree] =
+    new Functor[Tree] {
+      def map[A, B](tree: Tree[A])(func: A => B): Tree[B] =
+        tree match {
+          case Branch(left, right) =>
+            Branch(map(left)(func), map(right)(func))
+          case Leaf(value) =>
+            Leaf(func(value))
+        }
+    }
+
+
+  println(Tree.leaf(100).map(_ * 2))
+  println(Tree.branch(Tree.leaf(10), Tree.leaf(20)).map(_ * 2))
+
+
+}
