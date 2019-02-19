cf2b54f53afb8d7626c213550bdea867eb962be3
Ryan McDougall <rp.mcdougall@gmail.com>
2019-02-18 19:06:32 -0600

    implemented monad for cats.Id

--- src/main/scala/rm/cats/Monad.scala
+++ src/main/scala/rm/cats/Monad.scala
@@ -8,3 +8,16 @@ trait Monad[F[_]] {
   def map[A, B](value: F[A])(func: A => B): F[B] =
     flatMap(value)(a => pure(func(a)))
 }
+
+object IdMonad {
+  import cats.Id
+
+  def pure[A](value: A): Id[A] =
+    value
+
+  def map[A, B](initial: Id[A])(func: A => B): Id[B] =
+    func(initial)
+
+  def flatMap[A, B](initial: Id[A])(func: A => Id[B]): Id[B] =
+    func(initial)
+}
